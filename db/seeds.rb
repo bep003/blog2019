27cf645431c3f4ee24871015e2fca7f03c6660c9
Elizaveta Bashkova <bashkova_l@list.ru>
2019-10-25 17:58:04 +0300

    Zanyatie 25.10

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(login: 'admin', admin: true, password: 'sadmin', password_confirmation: 'sadmin')
 100.times do
   Post.create(title: Faker::Lorem.sentence,
               author: Faker::Name.name,
-              body: Faker::Lorem.paragraph)
+              body: Faker::Lorem.paragraph,
+              user: user)
 end
